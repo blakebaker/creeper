d205c7dea75ce2d7515b1c248f57bee491a0a64e
b <bbakerpkt@gmail.com>
2017-04-24 21:34:01 -0700

    Adding splitter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var domain = req.headers.host,
+      subDomain = domain.split('.');
+  if(subDomain[0].length == 0){
+    subDomain = "This guy";
+  }
+  res.render('index', { subDomain: subDomain });
 });
 
 module.exports = router;
